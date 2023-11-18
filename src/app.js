4d711845707b3d9385324fce4d4777ae91530a7e
sp1lu <davide.rivolta@yahoo.it>
2023-11-18 16:09:11 +0100

    Build

--- src/app.js
+++ src/app.js
@@ -1,11 +1,11 @@
 // Import modules
-import './routes/router.js';
+import '/src/routes/router.js';
 
 // Import components
-import './components/header/header.js';
-import './components/navbar/navbar.js';
-import './components/work/work.js';
-import './components/heart/heart.js';
-import './components/experience/experience.js';
-import './components/pill/pill.js';
-import './components/footer/footer.js';
+import '/src/components/header/header.js';
+import '/src/components/navbar/navbar.js';
+import '/src/components/work/work.js';
+import '/src/components/heart/heart.js';
+import '/src/components/experience/experience.js';
+import '/src/components/pill/pill.js';
+import '/src/components/footer/footer.js';
--- src/routes/router.js
+++ src/routes/router.js
@@ -1,7 +1,7 @@
 // Import modules
-import { designRouter } from './design';
-import { codingRouter } from './coding';
-import { experienceRouter } from './experience';
+import { designRouter } from '/src/routes/design';
+import { codingRouter } from '/src/routes/coding';
+import { experienceRouter } from '/src/routes/experience';
 
 // Router
 export const route = (event) => {
@@ -12,31 +12,31 @@ export const route = (event) => {
 };
 
 const routes = {
-    404: './src/pages/404.html',
-    '/': './src/pages/design.html',
-    '/design': './src/pages/design.html',
-    '/unigeneration': './src/pages/unigeneration.html',
-    '/valore-energia': './src/pages/valore-energia.html',
-    '/illustrazioni': './src/pages/illustrations.html',
-    '/udog': './src/pages/udog.html',
-    '/youni': './src/pages/youni.html',
-    '/pegli-circus': './src/pages/pegli-circus.html',
-    '/logofolio': './src/pages/logofolio.html',
-    '/rivarolo-1': './src/pages/rivarolo-1.html',
-    '/hitman-3': './src/pages/hitman-3.html',
-    '/kobe-bryant': './src/pages/kobe-bryant.html',
-    '/videogame-spoilers': './src/pages/videogame-spoilers.html',
-    '/botteghe-storiche-genova': './src/pages/botteghe-storiche-genova.html',
-    '/assassins-creed-odyssey-poster': './src/pages/assassins-creed-odyssey.html',
-    '/casa-medievale-render': './src/pages/casa-medievale-render.html',
-    '/re-kappa': './src/pages/kappa.html',
-    '/hard-surface-render': './src/pages/hard-surface.html',
-    '/coding': './src/pages/coding.html',
-    '/wordpress-theme': './src/pages/wordpress-theme.html',
-    '/newsly': './src/pages/newsly.html',
-    '/portfolio': './src/pages/design.html',
-    '/experience': './src/pages/experience.html',
-    '/about': './src/pages/about.html'
+    404: '/src/pages/404.html',
+    '/': '/src/pages/design.html',
+    '/design': '/src/pages/design.html',
+    '/unigeneration': '/src/pages/unigeneration.html',
+    '/valore-energia': '/src/pages/valore-energia.html',
+    '/illustrazioni': '/src/pages/illustrations.html',
+    '/udog': '/src/pages/udog.html',
+    '/youni': '/src/pages/youni.html',
+    '/pegli-circus': '/src/pages/pegli-circus.html',
+    '/logofolio': '/src/pages/logofolio.html',
+    '/rivarolo-1': '/src/pages/rivarolo-1.html',
+    '/hitman-3': '/src/pages/hitman-3.html',
+    '/kobe-bryant': '/src/pages/kobe-bryant.html',
+    '/videogame-spoilers': '/src/pages/videogame-spoilers.html',
+    '/botteghe-storiche-genova': '/src/pages/botteghe-storiche-genova.html',
+    '/assassins-creed-odyssey-poster': '/src/pages/assassins-creed-odyssey.html',
+    '/casa-medievale-render': '/src/pages/casa-medievale-render.html',
+    '/re-kappa': '/src/pages/kappa.html',
+    '/hard-surface-render': '/src/pages/hard-surface.html',
+    '/coding': '/src/pages/coding.html',
+    '/wordpress-theme': '/src/pages/wordpress-theme.html',
+    '/newsly': '/src/pages/newsly.html',
+    '/portfolio': '/src/pages/design.html',
+    '/experience': '/src/pages/experience.html',
+    '/about': '/src/pages/about.html'
 };
 
 const handleLocation = async () => {
